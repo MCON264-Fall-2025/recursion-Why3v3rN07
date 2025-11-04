cefdb24a3c6e8cbc03f345dc5240e1c13bf8b8aa
Why3v3rN07 <hfogarty@student.touro.edu>
2025-11-04 17:06:57 -0500

    In-class assignment

--- recursion/src/main/java/demos/LinkedListReversePrinter.java
+++ recursion/src/main/java/demos/LinkedListReversePrinter.java
@@ -20,6 +20,7 @@ public class LinkedListReversePrinter {
         head.next = new Node(20);
         head.next.next = new Node(30);
         head.next.next.next = new Node(40);
+        head.next.next.next.next = new Node(50);
 
         System.out.println("List in reverse:");
         printReverse(head);
